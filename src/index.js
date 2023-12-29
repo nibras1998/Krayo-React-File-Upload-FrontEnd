f2ad69e08374e59eea138e246991a842f4078a74
nibras1998 <mohdnibras1998@gmail.com>
2023-12-29 08:11:34 +0530

    Update index.js client Google secret

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-          <GoogleOAuthProvider clientId="6547637589-2iba3s4npuik7tdgshi84e3s0si3l247.apps.googleusercontent.com">
+          <GoogleOAuthProvider clientId="client-secret">
           <App />
           </GoogleOAuthProvider>
 
